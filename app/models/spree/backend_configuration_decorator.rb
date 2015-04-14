e57a750c246521077dd3e636dcbeb523612446ba
bvnbase <bvnbase@gmail.com>
2015-04-14 14:44:40 +0700

    + Update navigation

--- app/models/spree/backend_configuration_decorator.rb
+++ app/models/spree/backend_configuration_decorator.rb
@@ -1,5 +1,7 @@
 module Spree
   
+  BackendConfiguration.const_set( :PRODUCT_TABS, [ :products, :dish_types, :day_deliveries, :ingredients, 
+                                                   :what_you_needs, :nutritions, :product_nutritions, :images, :expert, :ingredientimages])
   BackendConfiguration.class_eval do
     
   end
