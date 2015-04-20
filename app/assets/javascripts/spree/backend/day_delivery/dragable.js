a863b6f885054f2bcb9d00427f6c01c8762914ae
bvnbase <bvnbase@gmail.com>
2015-04-20 17:26:22 +0700

    + Update javascript

--- app/assets/javascripts/spree/backend/day_delivery/dragable.js
+++ app/assets/javascripts/spree/backend/day_delivery/dragable.js
@@ -1,333 +1,180 @@
-$(document).ready(function () {
-  'use strict';
-  var week = 0;
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({ manualStart: true })
+  .on('move', function (event) {
+    var interaction = event.interaction;
 
-  var objToday = $(document).find("#server-date");
-  var today = objToday.attr("data-date");
+    // if the pointer was moved while being held down
+    // and an interaction hasn't started yet
+    if (interaction.pointerIsDown && !interaction.interacting()) {
+      var original = event.currentTarget,
+          // create a clone of the currentTarget element
+          clone = event.currentTarget.cloneNode(true);
 
-  LoadWeekData(0, today);
+      var width = original.offsetWidth
+          height = original.offsetHeight;
 
-  //Add select collection
-  $(document).find('.dishpicker').dishPicker();
+      // clone.style.Width = width;
+      // clone.style.Heigh = height;
+      // console.log(width + ", " + height);
 
-  //left button
-  var leftButton = $(document).find("#left-button");
-  leftButton.on("click", function(){
-    console.log("Click");
-    if(week > -1) 
-    {
-      week--;
-      LoadWeekData(week, today);
-    } 
-  });
+      clone.classList.add("dragging");
 
-  //right button
-  var rightButton = $(document).find("#right-button");
-  rightButton.on("click", function(){
-    if(week < 1) 
-    {
-      week++;
-      LoadWeekData(week, today);
+      original.parentNode.insertBefore(clone, original);
+      original.parentNode.removeChild(original);
+      // insert the clone to the page
+      // TODO: position the clone appropriately
+
+      // start a drag interaction targeting the clone
+      interaction.start({ name: 'drag' },
+                        event.interactable,
+                        clone);
     }
+  })
+  .on('dragmove', function (event) {
+
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  })
+  .on('dragend', function (event) {
+    var draggableElement = event.target;
+    draggableElement.classList.add('docked');
+    draggableElement.classList.remove('dragging');
+    draggableElement.removeAttribute("style");
+    draggableElement.removeAttribute('data-x');
+    draggableElement.removeAttribute('data-y');
   });
 
-  $(".close-button").on("click", function() {
-    console.log("Click");
+//=============================
+//=============================
+interact('.dropzone').dropzone({
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
 
-    var dishUI = $(this).parent();
-    var dishContainerUI = $(dishUI).parent();
+  // listen for drop related events:
 
-    var dishid = $(dishUI).attr("data-dishid");
-    var date = $(dishContainerUI).attr("data-date");
+  ondropactivate: function (event) {
 
-    //Send api
-    DeleteDishFromDateDelivery(dishid, date);
-    //Clear dish
-    dishUI.remove();
+    var draggableElement = event.target;
 
-  });
+    spaceElement = document.createElement("div");
+    spaceElement.classList.add("dish","drag-drop","space");
+    // add active dropzone feedback
+    draggableElement.classList.add('drop-active');
 
+    //console.log(draggableElement.parentNode);//.removeChild(draggableElement);
+
+  },
+  ondropmove: function (event) {
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    var cloneElement = draggableElement.cloneNode(true);
+    var dishesContainnerElement = dropzoneElement.getElementsByClassName("dishes-container");
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+    draggableElement.classList.add('can-drop');
+
+    //check exist space element
+    var listSpaceElement = dishesContainnerElement[0].getElementsByClassName("space");
+    var draggableOffset = draggableElement.getBoundingClientRect();
+    var containerOffset = dishesContainnerElement[0].getBoundingClientRect();
+    //console.log(listSpaceElement);
+    //onsole.log(spaceElement);
+    if (listSpaceElement.length == 0)
+    {
+      //console.log("SET");
+
+      //Calculate position
+      // var width = draggableElement.offsetWidth;
+      // var height = draggableElement.offsetHeight;
+
+      //Get absolute position
+
+      var distance = draggableOffset.top - containerOffset.top;
+
+      //console.log(distance);
+      var elementHeight = draggableElement.offsetHeight;
+
+      var index = parseInt(distance / elementHeight);
+      
+      dishesContainnerElement[0].insertBefore(spaceElement, dishesContainnerElement[0].childNodes[index + 1]);
+    }
+    else
+    {
+      
+      dishesContainnerElement[0].removeChild(listSpaceElement[0]);
+
+      //Calculate distance
+      var distance = draggableOffset.top - containerOffset.top;
+
+      //Get element height
+      var elementHeight = draggableElement.offsetHeight;
+
+      //Get index
+      var index = parseInt(distance / elementHeight);
+
+      dishesContainnerElement[0].insertBefore(spaceElement, dishesContainnerElement[0].children[index + 1]);
+    }
+    //dropzoneElement.appendChild(cloneElement);  
+  },
+  ondragleave: function (event) {
+    var draggableElement = event.relatedTarget,
+      dropzoneElement = event.target;
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+    event.relatedTarget.classList.remove('can-drop');
+
+    var dishesContainnerElement = dropzoneElement.getElementsByClassName("dishes-container");
+    var listSpaceElement = dishesContainnerElement[0].getElementsByClassName("space");
+
+    //Remove space if not drop
+    if (listSpaceElement.length != 0)
+    {
+      dishesContainnerElement[0].removeChild(listSpaceElement[0]);
+    }
+  },
+  ondrop: function (event) {
+    console.log("DROP");
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    // console.log(dropzoneElement);
+    var dishesContainnerElement = dropzoneElement.getElementsByClassName("dishes-container");
+
+    //check exist space element
+    var listSpaceElement = dishesContainnerElement[0].getElementsByClassName("space");
+    if (listSpaceElement.length != 0)
+    {
+      dishesContainnerElement[0].replaceChild(draggableElement, listSpaceElement[0]);
+
+      //Update date
+      var date = dishesContainnerElement[0].getAttribute("data-date");
+      var id = draggableElement.getAttribute("data-id");
+      
+      console.log(date + "," + id);
+
+      UpdateDateDelivery(id, date);
+
+    }
+    //Add new dish to dishes container
+    //dishesContainnerElement[0].appendChild( draggableElement );
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
 });
-
-function LoadWeekData(week, today)
-{
-
-  //UI
-  switch(week)
-  {
-    case 1:
-      $("#right-button").addClass("disabled");
-      $("#center-bar").addClass("next");
-    break;
-    case 0:
-      $("#center-bar").removeClass("last");
-      $("#center-bar").removeClass("next");
-      $("#left-button").removeClass("disabled");
-      $("#right-button").removeClass("disabled");
-    break;
-    case -1:
-      $("#center-bar").addClass("last");
-      $("#left-button").addClass("disabled");
-    break;
-  }
-
-
-  var time = CalculateTimeByWeek(week, today);
-
-  console.log(time.firstDate + ", " + time.lastDate);
-
-
-  var centerbar = document.getElementById("center-bar");
-  var date_start = centerbar.getAttribute("data-datestart");
-  var date_end = centerbar.getAttribute("data-dateend");
-
-  var dishContainer = $(document).find(".dishes-container");
-
-  for (i = 0 ; i < dishContainer.length; i++)
-  {
-    var date = new Date(time.firstDate);
-    date = date.setDate( date.getDate() + i);
-    $(dishContainer[i]).attr("data-date", TimeFormat(date));
-  }
-
-  $(centerbar).attr("data-datestart", TimeFormat(time.firstDate));
-  $(centerbar).attr("data-dateend", TimeFormat(time.lastDate));
-
-  $(centerbar).text(TimeFormat(time.firstDate) + " ==> " + TimeFormat(time.lastDate));
-
-  //Load data
-  LoadDataDateDelivery(TimeFormat(time.firstDate), TimeFormat(time.lastDate));
-}
-
-function CalculateTimeByWeek(week, today)
-{
-  var date = new Date(today);
-
-  date.setDate(date.getDate() + 7 * week - date.getDay() + 1);
-  var firstDate = new Date(date);
-
-  date.setDate(date.getDate() + 5);
-  var lastDate = new Date(date);
-
-  return {
-    firstDate: firstDate,
-    lastDate: lastDate,
-  };
-}
-
-function TimeFormat(time)
-{
-  var timeFormat = new Date(time);
-
-  var d = timeFormat.getDate();
-
-  if( d < 10 )
-  {
-    d = "0" + d;
-  }
-
-  var m = timeFormat.getMonth() + 1;
-
-  if( m < 10 )
-  {
-    m = "0" + m;
-  }
-
-  var y = timeFormat.getFullYear();
-
-  return y + "-" + m + "-" + d;
-}
-
-function AddDateDelivery(dishId, date)
-{
-  var apiUrl = Spree.routes.date_delivery_api;
-  var recordId;
-
-  $.ajax({
-    url: apiUrl,
-    type: 'post',
-    async: false,       //NOTE
-    data: 
-    {
-      token: Spree.api_key,
-      date_delivery: {
-        product_ids: dishId,
-        delivery_date: date
-      }
-    },
-    success: function(result)
-    {
-      recordId = result.id;
-    },
-    error: function()
-    {
-
-    }
-  });
-
-  return recordId;
-}
-
-function UpdateDateDelivery(recordId, date)
-{
-  var apiUrl = Spree.routes.date_delivery_api;
-
-  $.ajax({
-    url: apiUrl,
-    type: 'put',
-    //async: false,       //NOTE
-    data: 
-    {
-      token: Spree.api_key,
-      date_delivery: {
-        id: recordId,
-        delivery_date: date
-      }
-    },
-    success: function(result)
-    {
-      console.log("Update successfully!");
-    },
-    error: function()
-    {
-
-    }
-  });
-}
-
-function LoadDataDateDelivery(date_start, date_end)
-{
-  var dishContainer;
-  var html;
-  var dish;
-
-  var data = GetDataByRangeOfDate(date_start, date_end);
-  console.log(data);
-
-  dishContainer = $(document).find(".dishes-container");
-  dishContainer.empty()
-  for (i = 0; i < data.length; i++)
-  {
-    dishes = data[i].products;
-    var date = data[i]["delivery_date"];
-
-    dishContainer = $(document).find(".dishes-container[data-date='" + date + "']")
-    dishContainer.empty();
-    //console.log(dishContainer);
-
-    for (k = 0; k < dishes.length; k++)
-    {
-      //console.log(k);
-      var dish = dishes[k];
-
-      var imgUrl = "";
-
-      if(dish.images.length > 0)
-      {
-        imgUrl = dish.images[0].product_url;
-      }
-
-      html = DishItemFormat(dish.pd_id, dish.id, imgUrl, dish.name);
-      //console.log(html);
-      $(dishContainer).append(html);
-    }
-  }
-}
-
-/*--------------------*/
-/*  GET DATA          */
-/*--------------------*/
-function GetDataByRangeOfDate(dateStart, dateEnd)
-{
-  var apiUrl = Spree.routes.date_delivery_duration_api;
-  var dataResult;
-  $.ajax(
-  {
-    url: apiUrl,
-    async: false,
-    data: 
-    {
-      start_date: dateStart,
-      end_date: dateEnd
-    },
-    success: function(result)
-    {
-      //console.log(result);
-      dataResult = result;
-    },
-    error: function()
-    {
-
-    }
-  });
-
-  return dataResult;
-}
-
-function DishItemFormat(id, dishid, imgUrl, name)
-{
-  var html = "\
-    <div class='dish draggable drag-drop docked' data-id='" + id + "' data-dishid='" + dishid + "'>\
-      <div class='close-button'>\
-        <i class='fa fa-times'></i>\
-      </div>\
-      <div class='image-container'>\
-        <img class='image-dish' src='" + imgUrl + "'>\
-      </div>\
-      <p class='name'>" + name + "</p>\
-    </div>\
-  ";
-
-  return html;
-}
-
-/*--------------------*/
-/*  UPDATE            */
-/*--------------------*/
-function UpdateDateDelivery(id, date)
-{
-  var apiUrl = Spree.routes.date_delivery_api + "/" + id;
-  $.ajax({
-    url: apiUrl,
-    type: 'put',
-    data: 
-    {
-      token: Spree.api_key,
-      delivery_date: date
-    },
-    success: function(result)
-    {
-      console.log(result);
-    },
-    error: function()
-    {
-
-    }
-  });
-}
-
-
-/*--------------------*/
-/*  DELETE            */
-/*--------------------*/
-function DeleteDishFromDateDelivery(dishid, date)
-{
-  var apiUrl = Spree.routes.product_search + "/" + dishid + "/remove_date";
-  $.ajax({
-    url: apiUrl,
-    type: 'delete',
-    data: 
-    {
-      token: Spree.api_key,
-      delivery_date: date
-    },
-    success: function(result)
-    {
-      console.log(result);
-    },
-    error: function(data)
-    {
-      console.log(data);
-    }
-  });
-}
