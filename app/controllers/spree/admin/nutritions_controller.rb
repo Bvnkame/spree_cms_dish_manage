2c1132ab3244fa8088025091df08c72fa3c6b627
bvnbase <bvnbase@gmail.com>
2015-05-08 09:08:42 +0700

    + Add pagination to nutritions, what you needs, and ingredients

--- app/controllers/spree/admin/nutritions_controller.rb
+++ app/controllers/spree/admin/nutritions_controller.rb
@@ -6,6 +6,10 @@ module Spree
         Dish::Nutrition
       end
 
+      def index
+        respond_with(@collection)
+      end
+
       def show
         redirect_to action: :edit
       end
@@ -17,6 +21,24 @@ module Spree
         admin_nutritions_url
       end
 
+      def collection
+        return @collection if @collection.present?
+        params[:q] ||= {}
+        params[:q][:deleted_at_null] ||= "1"
+
+        params[:q][:s] ||= "name asc"
+        @collection = super
+        if params[:q].delete(:deleted_at_null) == '0'
+          @collection = @collection.with_deleted
+        end
+        # @search needs to be defined as this is passed to search_form_for
+        @search = @collection.ransack(params[:q])
+        @collection = @search.result.
+              page(params[:page]).
+              per(params[:per_page] || Spree::Config[:admin_products_per_page])
+
+        @collection
+      end
     end
   end
 end
--- app/controllers/spree/admin/what_you_needs_controller.rb
+++ app/controllers/spree/admin/what_you_needs_controller.rb
@@ -6,6 +6,10 @@ module Spree
         Dish::Whatneed
       end
 
+      def index
+        respond_with(@collection)
+      end
+
       def show
         redirect_to action: :edit
       end
@@ -15,6 +19,25 @@ module Spree
 
       def location_after_save
         admin_what_you_needs_url
+      end
+
+      def collection
+        return @collection if @collection.present?
+        params[:q] ||= {}
+        params[:q][:deleted_at_null] ||= "1"
+
+        params[:q][:s] ||= "name asc"
+        @collection = super
+        if params[:q].delete(:deleted_at_null) == '0'
+          @collection = @collection.with_deleted
+        end
+        # @search needs to be defined as this is passed to search_form_for
+        @search = @collection.ransack(params[:q])
+        @collection = @search.result.
+              page(params[:page]).
+              per(params[:per_page] || Spree::Config[:admin_products_per_page])
+
+        @collection
       end
     end
   end
